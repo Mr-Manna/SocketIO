08639afbc521b8f28a2a186558412fddfce18249
mr_manna <mr.manna87@gmail.com>
2020-03-05 13:10:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const port = 3000;
+
+
+server.listen(port,()=>{ `server is running on ${port}`});
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+io.on('connection',(socket)=>{
+    console.log("user connected");
+    socket.emit('message', { 'chan':"how are you?"});
+    socket.on('another event',(data)=>{
+        console.log(data);
+    })
+})
